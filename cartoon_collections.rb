aaef4b0b9382b6ca8c7c61c372ce50fbf6899cd4
Matt Smith <mrandolph83@gmail.com>
2020-10-13 13:58:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |calls|
-    calls.upcase# code an argument here
+    calls.upcase + "!"# code an argument here
   # Your code here
 end
 end
